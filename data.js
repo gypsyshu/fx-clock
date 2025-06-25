3fb29fdc4776b42ff83a864ec3daf2da8d346e6c
gypsyshu <gypsy@example.com>
2025-06-25 09:13:53 +0900

    update: 最新ログでdata.jsを再生成

--- data.js
+++ data.js
@@ -1,15 +1,14 @@
 const tradeLog = [
   {
     "date": "2025-06-24",
-    "result": 1,
+    "result": "1",
     "pips": 30.0,
     "rr": 2.0
   },
   {
     "date": "2025-06-24",
-    "result": 0,
+    "result": "0",
     "pips": -15.0,
     "rr": 0.5
   }
 ];
-const balance = 231000;

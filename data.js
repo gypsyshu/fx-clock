06f6e739811d2f5763aa852cdedaba80bf2b3d07
gypsyshu <gypsyshu@gmail.com>
2025-06-25 09:07:53 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const tradeLog = [
+  {
+    "date": "2025-06-24",
+    "time": "21:35",
+    "result": 1,
+    "pips": 30.0,
+    "rr": 2.0
+  },
+  {
+    "date": "2025-06-24",
+    "time": "22:10",
+    "result": 0,
+    "pips": -15.0,
+    "rr": 0.5
+  }
+];
+
+const balance = 231000;
